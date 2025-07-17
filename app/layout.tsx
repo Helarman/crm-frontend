cbc4d2531fa4016f317d8598482b19e4dae2fcc9
Helarman <helarman@bk.ru>
2025-07-17 11:49:34 +0300

    fix theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,8 +19,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <ThemeProvider
             attribute="class"
-            defaultTheme="system"
-            enableSystem
+            defaultTheme="light"
             disableTransitionOnChange
           >
             <AuthInitializer>
