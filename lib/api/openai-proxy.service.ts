5ac71120c257d580229ec916f48f7aaaba5d772d
Helarman <helarman@bk.ru>
2025-10-24 14:24:35 +0300

    proxy url

--- lib/api/openai-proxy.service.ts
+++ lib/api/openai-proxy.service.ts
@@ -2,7 +2,7 @@ class OpenAIService {
   private proxyBaseUrl: string;
 
   constructor() { 
-    this.proxyBaseUrl = 'http://localhost:3001';
+    this.proxyBaseUrl = 'https://jukirililuk.beget.app';
   }
 
   async makeRequest(endpoint: string, body: any, apiKey?: string) {
